ade709dbe6417a966c2c7ed1fd5257b58c05e3b4
rober0xf <robertinopili13@gmail.com>
2025-03-19 18:10:29 -0300

    (MODIFIED) xml parser && (ADD) json structure

--- Makefile
+++ Makefile
@@ -1,17 +1,20 @@
 CXX = clang++
-CXXFLAGS = -Wall -std=c++17 -I/usr/local/include  
+CXXFLAGS = -Wall -std=c++17 -I/usr/local/include  -I./include
 LDFLAGS = -lpugixml
-SRC = src/main.cpp
-OBJ = build/main.o
+SRC = src/main.cpp src/lexer.cpp
+OBJ = build/main.o build/lexer.o
 EXE = build/program
 
 all: $(EXE)
 
-build/main.o: src/main.cpp
+build/main.o: src/main.cpp include/lexer.hpp
 	$(CXX) $(CXXFLAGS) -c src/main.cpp -o build/main.o
 
-$(EXE): build/main.o 
-	$(CXX) build/main.o -o $(EXE) $(LDFLAGS)
+build/lexer.o: src/lexer.cpp include/lexer.hpp
+	$(CXX) $(CXXFLAGS) -c src/lexer.cpp -o build/lexer.o
+
+$(EXE): $(OBJ) 
+	$(CXX) $(OBJ) -o $(EXE) $(LDFLAGS)
 
 clean:
 	rm -f build/*.o build/program 
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,106 @@
+#include <algorithm>
+#include <cstddef>
 #include <cstring>
+#include <filesystem>
+#include <fstream>
 #include <iosfwd>
 #include <iostream>
+#include <json.hpp>
 #include <ostream>
 #include <pugixml.hpp>
 #include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
-void parse_xml_file(const std::string& path);
-void read_node(const pugi::xml_node& node);
+#include "../include/lexer.hpp"
 
+template <typename Key, typename Value>
+using unordered_map = std::unordered_map<Key, Value>;
+
+std::string parse_xml_file(const std::string& path);
+unordered_map<std::string, size_t> index_document(std::string content);
+
+namespace fs = std::filesystem;
+using json = nlohmann::json;
 int main() {
-    std::string file_path = "docs.gl/gl3/glPixelStore.xhtml";
-    parse_xml_file(file_path);
+    unordered_map<fs::path, unordered_map<std::string, size_t>> all_documents;
+    std::string dir_path = "docs.gl/gl4";
+
+    json index_json;
+    for (const auto& entry : fs::directory_iterator(dir_path)) {
+        if (entry.is_regular_file()) {
+            fs::path file_path = entry.path();
+            std::cout << "processing file: " << file_path << std::endl;
+
+            try {
+                std::string content = parse_xml_file(file_path.string());
+                if (content.empty()) {
+                    continue;
+                }
+
+                Lexer lexer(content);
+                std::unordered_map<std::string, size_t> frequency_table;
+
+                std::optional<std::string_view> token;
+                while ((token = lexer.next_token())) {
+                    std::string tokenStr(token->data(), token->size());
+                    frequency_table[tokenStr]++;
+                }
+
+                all_documents[file_path] = std::move(frequency_table);
+            } catch (const std::exception& e) {
+                std::cerr << "exception processing file: " << e.what() << std::endl;
+                std::cerr << "file: " << file_path << std::endl;
+                continue;
+            }
+        }
+    }
+
+    for (const auto& doc : all_documents) {
+        std::cout << "file: " << doc.first << std::endl;
+        std::vector<std::pair<std::string, size_t>> freq_vec(doc.second.begin(),
+                                                             doc.second.end());
+        std::sort(freq_vec.begin(), freq_vec.end(),
+                  [](const auto& a, const auto& b) { return a.second > b.second; });
+        json top_terms = json::array();
+        size_t top_ten = 0;
+        for (const auto& term : freq_vec) {
+            std::cout << "  " << term.first << "--> " << term.second << std::endl;
+            top_terms.push_back({{"term", term.first}, {"frequency", term.second}});
+            top_ten++;
+            if (top_ten >= 10) break;
+        }
+
+        index_json[doc.first] = top_terms;
+        std::cout << std::endl;
+    }
+
+    std::ofstream output_file("index.json");
+    if (output_file.is_open()) {
+        output_file << index_json.dump(4);  // pretty print 4 spaces
+        output_file.close();
+        std::cout << "JSON written successfully" << std::endl;
+    } else {
+        std::cerr << "failed to writing JSON file" << std::endl;
+    }
 
     return 0;
 }
 
-void parse_xml_file(const std::string& path) {
+std::string parse_xml_file(const std::string& path) {
     pugi::xml_document doc;
-    pugi::xml_parse_result result =
-        doc.load_file(path.c_str());  // ::load_file takes a char*
+
+    // ::load_file takes a char*
+    pugi::xml_parse_result result = doc.load_file(path.c_str());
     if (!result) {
         std::cerr << "ERROR: " << result.description() << std::endl;
+        return {};
     }
 
-    read_node(doc.document_element());
-}
+    std::stringstream ss;
+    doc.save(ss);
 
-void read_node(const pugi::xml_node& node) {
-    for (const pugi::xml_node& child : node.children()) {
-        if (std::string(child.child_value()).empty() && child.first_attribute().empty() &&
-            std::string(child.value()).empty()) {
-            continue;
-        }
-        for (const pugi::xml_attribute& attr : child.attributes()) {
-            std::cout << attr.name() << ": " << attr.value() << " ";
-        }
-        std::cout << child.text().as_string() << std::endl;
-
-        // read_node(child);
-    }
+    return ss.str();
 }
